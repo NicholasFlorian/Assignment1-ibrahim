d0822f7fd9383d8dcb6bd291e982f81ecaccac95
Nick <nickjflorian@Gmail.com>
2017-10-15 20:02:46 -0400

    Update 2
    Fixed line spacing

--- word_checker.c
+++ word_checker.c
@@ -38,13 +38,19 @@ int abidesRules(int i, int j, char *word, struct rolledDice** gameBoard, int sub
         newY = j + allyVariable[adjCell];
         
         if((newX >= 0) && (newX < 4) && (newY >=0) && (newY < 4) && toupper(findLetter) == getLetter(newX, newY, gameBoard) && !visited[newX][newY]){
+            
             visited[newX][newY] = 1;
             ++subLen;
             result = abidesRules(newX, newY, word, gameBoard, subLen, visited);
+            
             if(result){
+                
                 return 1;
+                
             }else{
+                
                 --subLen;
+                
             }
             
         }
@@ -71,23 +77,32 @@ int wordChecker(struct rolledDice **gameBoard, char *word)
     for(m = 0; m < 4; m++){
         for(n = 0; n < 4; n++){
             if(m == 0 && n == 0){
+                
                 visited[m][n] = 1;
+                
             }else{
+                
                 visited[m][n] = 0;
+                
             }
         }
     }
     
     for (int row = 0; row < 4; row++) {
+        
         for (int col = 0; col < 4; col++) {
+            
             if(abidesRules(row, col, word, gameBoard, 0, visited)){
+                
                 free(visited[0]);
                 free(visited[1]);
                 free(visited[2]);
                 free(visited[3]);
+                
                 return 1;
                 
             }
+            
         }
         
     }
@@ -111,6 +126,7 @@ char testGetLetter(int i, int j, char **boggle)
 int testAbidesRules(int i, int j, char *word, char **gameBoard, int subLen, int **visited)
 {
     int adjCell;
+    
     // use subLen as index to see what letter to find
     char currentLetter = word[subLen];
     int ans = (subLen == (strlen(word)-1));
@@ -125,32 +141,45 @@ int testAbidesRules(int i, int j, char *word, char **gameBoard, int subLen, int 
     
     int result = 0;
     
-    if(subLen == (strlen(word)-1)){
+    
+    if(subLen == (strlen(word) -1)){
         return 1;
     }
     
     if(toupper(currentLetter) == 'Q' && toupper(word[subLen + 2]) == testGetLetter(i,j,gameBoard)){
+        
         findLetter = word[subLen + 3];
         subLen++;
         subLen++;
         currentLetter = word[subLen];
+        
     }else{
+        
         findLetter = word[subLen + 1];
+        
     }
     
     for(adjCell = 0; adjCell < 8; adjCell++){
+        
         newX = i + allxVariable[adjCell];
         newY = j + allyVariable[adjCell];
+        
         if((newX >= 0) && (newX < 4) && (newY >=0) && (newY < 4)
            && toupper(findLetter) == testGetLetter(newX, newY, gameBoard)
            && !visited[newX][newY]){
+            
             visited[newX][newY] = 1;
             ++subLen;
             result = testAbidesRules(newX, newY, word, gameBoard, subLen, visited);
+            
             if(result){
+                
                 return 1;
+                
             }else{
+                
                 --subLen;
+                
             }
         }
     }
@@ -170,22 +199,32 @@ int testWordChecker(char **boggle, char *word)
     visited[3] = malloc(sizeof(int) * 4);
     
     for(m = 0; m < 4; m++){
+        
         for(n = 0; n < 4; n++){
+            
             if(m == 0 && n == 0){
+                
                 visited[m][n] = 1;
+                
             }else{
+                
                 visited[m][n] = 0;
+                
             }
         }
     }
     
     for (int row = 0; row < 4; row++) {
+        
         for (int col = 0; col < 4; col++) {
+            
             if(testAbidesRules(row, col, word, boggle, 0, visited)){
+                
                 free(visited[0]);
                 free(visited[1]);
                 free(visited[2]);
                 free(visited[3]);
+                
                 return 1;
                 
             }
@@ -217,20 +256,28 @@ int hcWordChecker(char boggle[][4], char *word)
     for(m = 0; m < 4; m++){
         for(n = 0; n < 4; n++){
             if(m == 0 && n == 0){
+                
                 visited[m][n] = 1;
+                
             }else{
+                
                 visited[m][n] = 0;
+                
             }
         }
     }
     
     for (int row = 0; row < 4; row++) {
+        
         for (int col = 0; col < 4; col++) {
+            
             if(hcAbidesRules(row, col, word, boggle, 0, visited)){
+                
                 free(visited[0]);
                 free(visited[1]);
                 free(visited[2]);
                 free(visited[3]);
+                
                 return 1;
                 
             }
@@ -263,34 +310,45 @@ int hcAbidesRules(int i, int j, char *word, char boggle[][4], int subLen, int **
     char findLetter;
     
     if(subLen == (strlen(word)-1)){
+        
         return 1;
+        
     }
     
     if(toupper(currentLetter) == 'Q' && toupper(word[subLen + 2]) == hcGetLetter(i,j,boggle)){
+        
         findLetter = word[subLen + 3];
         subLen++;
         subLen++;
         currentLetter = word[subLen];
         
     }else{
+        
         findLetter = word[subLen + 1];
     }
     
     
     for(adjCell = 0; adjCell < 8; adjCell++){
-        int newX = i + allxVariable[adjCell];
-        int newY = j + allyVariable[adjCell];
+        
+        newX = i + allxVariable[adjCell];
+        newY = j + allyVariable[adjCell];
+        
         if((newX >= 0) && (newX < 4) && (newY >=0) && (newY < 4)
            && toupper(findLetter) == hcGetLetter(newX, newY, boggle)
            && !visited[newX][newY]){
+            
             visited[newX][newY] = 1;
             ++subLen;
             result = hcAbidesRules(newX, newY, word, boggle, subLen, visited);
             
             if(result){
+                
                 return 1;
+                
             }else{
+                
                 --subLen;
+                
             }
             
         }
